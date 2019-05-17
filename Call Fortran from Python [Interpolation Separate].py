Fixed htro3 layering, moved cloudcol column flux weighting
- Added more plots to 'read and plot'
- Made A_oz_l use values of A_oz_x from the correct levels
- Moved initial setup convection inside cloudcol do loop
- Added x = xwghtd outside cloudcol loop
- Added extra htrm speed-ups
- Added outside control of meridtransp method
- Uncommented htr(nlayers) = 0.0

Call Fortran from Python [Interpolation Separate].py
@@ -20,13 +20,13 @@ from scipy import stats
 
 # ncols = 31
 # ncolss = np.linspace(3,11,5)
-ncolss = [21]
+ncolss = [1]
 
 for ncols in ncolss:
 
     ncols = int(ncols)
 
-    nlays = 30
+    nlays = 150
     days = 5000 #model days
 
     def create_misr_cloud_inputs():
@@ -385,7 +385,7 @@ for ncols in ncolss:
     for cld_height in cld_heights:
 
         manual_clouds = []
-        manual_clouds.append([cld_height,0.5,0.0])
+        manual_clouds.append([2.0,0.5,0.0])
         ncloudcols = shape(manual_clouds)[0]
 
         for tboundm in tboundms:
@@ -433,16 +433,17 @@ for ncols in ncolss:
                         #lc[0] = -10.0
                         #lc[ncols-1] = -10.0
                         #for i in range(len(lc)):
-                        #    lc[i] = lcmean
+                        #   lc[i] *= 1.5
 
                         lch = createlatdistbn('Cloud Top Height')
                         srh = createlatdistbn('Relative Humidity')
                         # srh = [0.8] * ncols
-                        #sa = createlatdistbn('Surface Reflectance')
-                        sa = [0.3] * ncols
+                        sa = createlatdistbn('Surface Reflectance')
+                        # sa = [0.3] * ncols
                         lcf = createlatdistbn('Cloud Fraction')
                         lcod = createlatdistbn('Cloud Optical Thickness')
                         tg = createlatdistbn('Surface Temperature')
+
                         # tg = [tboundm] * ncols
                         # tg = [250.] * ncols
                     
@@ -466,7 +467,7 @@ for ncols in ncolss:
                         #lct = 250.0
                         #lcf = 0.5
                         #lcod = 5.0
-                        tp = 5.0
+                        tp = 5.0 * 1e3
                         #fth = np.zeros(ncols)
                         #for i in range(ncols):
                         #    fth[i] = 15.0 - abs(collats[i])/18.0
@@ -494,7 +495,7 @@ for ncols in ncolss:
                         htransp = 1.0 #reduce lapse rate to account for horizontal transport
                         ipe = 1
                         dp = 1
-                        mtranspfac = 2.0 * 0.0
+                        mtranspfac = 2.0
                         boxnetfluxfac = 0.2
                         twarm = 288
                         tcold = 268
@@ -504,10 +505,10 @@ for ncols in ncolss:
                         eta = 0.75
                         planet_radius = 6.37e6
                         planet_rotation = 7.29e-5
-                        t_min = 100.0
+                        t_min = 180
                         sfc_heating = 0 #surface energy budget warms/cools surface? 1=yes, 0=no
                         playtype = 0 #pressure layer type. 0=equal p thickness, 1=sigma
-                        ur_htr = 0.5
+                        ur_htr = 0.1
                         ur_toafnet = 3.0
                         ur_seb = 1e10
                         couple_tgta = 1

Read and plot 2D RCM output data (compare multiple files).py
@@ -178,7 +178,7 @@ def readfile(fn,counter):
     # abs_surf = np.mean(abs_surf_lhcols[0,:]) / factor
 
 
-    return tzmcols,pzmcols,wklm1cols,totuflumcols,htrmcols,altzmcols,pavelmcols,htro3cols,totdflumcols,wklm2cols,A_oz_lcols,abspncols,abs_surf_lhcols,tboundmcols,tavelmcols,nlayersm,ncols,boxlatcols
+    return tzmcols,pzmcols,wklm1cols,totuflumcols,htrmcols,altzmcols,pavelmcols,htro3cols,totdflumcols,wklm2cols,A_oz_lcols,abspncols,abs_surf_lhcols,tboundmcols,tavelmcols,nlayersm,ncols,boxlatcols,htrh2ocols,wklm1cols
 
 # nlayersms=[31,100]
 # ncols=5
@@ -212,10 +212,12 @@ for directory in directories:
     for fn in a:
         if (fn == '.DS_Store' or fn == 'new benchmark'):
             continue
-        tzmcols,pzmcols,wklm1cols,totuflumcols,htrmcols,altzmcols,pavelmcols,htro3cols,totdflumcols,wklm2cols,A_oz_lcols,abspncols,abs_surf_lhcols,tboundmcols,tavelmcols,nlayersm,ncols,boxlatcols = readfile(fn,counter)
+        tzmcols,pzmcols,wklm1cols,totuflumcols,htrmcols,altzmcols,pavelmcols,htro3cols,totdflumcols,wklm2cols,A_oz_lcols,abspncols,abs_surf_lhcols,tboundmcols,tavelmcols,nlayersm,ncols,boxlatcols,htrh2ocols,wklm1cols = readfile(fn,counter)
         tzm_master.append(tzmcols)  
         pzm_master.append(pzmcols)
         boxlatcols_master.append(boxlatcols)
+
+        htrmlwcols = htrmcols[1:,:] - htro3cols - htrh2ocols
 
         i3=0
 
@@ -223,48 +225,80 @@ for directory in directories:
 
             for col in range(ncols):
 
-                plt.figure(1)
-                plt.subplot(331)
+                plt.figure(i2+1)
+                plt.subplot(341)
                 plt.title('tzm')
                 plt.semilogy(tzmcols[:,col],pzmcols[:,col],'-o',label=str(fn))
                 # plt.semilogy(tzmcols[:,col],pzmcols[:,col],label=str(fn))
                 # plt.plot(tzmcols[:,col],altzmcols[:,col],'-o',label=str(fn))
                 plt.plot(tboundmcols[0,col],pzmcols[0,col],'*',markersize=20)
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
                 # plt.legend()
                 
-                plt.subplot(332)
+                plt.figure(i2+1)
+                plt.subplot(342)
                 plt.title('htrm')
-                plt.semilogy(htrmcols[:,col],pzmcols[:,col])
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.semilogy(htrmcols[:,col],pzmcols[:,col],'-o')
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
                 plt.axvline(-0.03,ls='--')
                 plt.axvline(0.03,ls='--')
                 
-                plt.subplot(333)
+                plt.figure(i2+1)
+                plt.subplot(343)
                 plt.title('totuflum')
                 plt.semilogy(totuflumcols[:,col],pzmcols[:,col])
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
                 
-                plt.subplot(334)
-                plt.title('totdfllum')
+                plt.figure(i2+1)
+                plt.subplot(344)
+                plt.title('totdflum')
                 plt.semilogy(totdflumcols[:,col],pzmcols[:,col])
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
 
-                plt.subplot(335)
+                plt.figure(i2+1)
+                plt.subplot(345)
                 plt.title('abs_o3')
                 plt.semilogy(A_oz_lcols[:,col]*1362./4.,pzmcols[1:,col])
-                plt.ylim(pzmcols[0,col]*1.1,1)      
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))      
 
-                plt.subplot(336)
+                plt.figure(i2+1)
+                plt.subplot(346)
                 plt.title('abspn')
                 plt.semilogy(abspncols[:,col]*1362./4.,pzmcols[1:,col])
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
 
-                plt.subplot(337)
+                plt.figure(i2+1)
+                plt.subplot(347)
                 plt.title('tavelm')
                 plt.semilogy(tavelmcols[:,col],pzmcols[1:,col],label=str(fn))
-                plt.ylim(pzmcols[0,col]*1.1,1)
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
                 # plt.legend()
+
+                plt.figure(i2+1)
+                plt.subplot(348)
+                plt.title('htro3')
+                plt.semilogy(htro3cols[:,col],pzmcols[1:,col],label=str(fn))
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
+
+                plt.figure(i2+1)
+                plt.subplot(349)
+                plt.title('htrh2o')
+                plt.semilogy(htrh2ocols[:,col],pzmcols[1:,col],label=str(fn))
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
+
+                plt.figure(i2+1)
+                plt.subplot(3,4,10)
+                plt.title('wklm1 (h2o)')
+                plt.loglog(wklm1cols[:,col],pzmcols[1:,col],label=str(fn))
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
+
+                plt.figure(i2+1)
+                plt.subplot(3,4,11)
+                plt.title('htrmlw')
+                plt.semilogy(htrmlwcols[:,col],pzmcols[1:,col])
+                plt.ylim(max(pzmcols[:,col]),min(pzmcols[:,col]))
+                plt.axvline(-0.03,ls='--')
+                plt.axvline(0.03,ls='--')
 
                 i3+=1
 
@@ -274,13 +308,13 @@ for directory in directories:
 
 
 # master indices: master[file][layer][column]
-tzm_master = np.array(tzm_master)
-pzm_master = np.array(pzm_master)
-boxlatcols_master = np.array(boxlatcols_master)
+# tzm_master = np.array(tzm_master)
+# pzm_master = np.array(pzm_master)
+# boxlatcols_master = np.array(boxlatcols_master)
 
-filenames = np.array(filenames[0])
+# filenames = np.array(filenames[0])
 
-x = np.linspace(-90,90,ncols)
+# x = np.linspace(-90,90,ncols)
 
 
 