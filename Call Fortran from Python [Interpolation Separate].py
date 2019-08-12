7cb23fce43669ec05e1e2c3c7decacca119be701
Nick Edkins (Home) <nedk437@aucklanduni.ac.nz>
2019-08-12 14:27:15 +1200

    Minor tweaks

--- Call Fortran from Python [Interpolation Separate].py
+++ Call Fortran from Python [Interpolation Separate].py
@@ -18,14 +18,12 @@ from os import listdir
 from time import localtime, strftime
 from scipy import stats
 
-project_dir = '/Users/nickedkins/Dropbox/GitHub Repositories/Uni/2D-RCM/'
-#project_dir = '/Users/nickedkins/Dropbox/GitHub Repositories/Home/2D-RCM/'
+# project_dir = '/Users/nickedkins/Dropbox/GitHub Repositories/Uni/2D-RCM/'
+project_dir = '/Users/nickedkins/Dropbox/GitHub Repositories/Home/2D-RCM/'
 
-# ncols = 31
-# ncolss = np.linspace(3,11,5)
-ncolss = [1]
-ncloudcols = 2
-nlays = 199
+ncolss = [3]
+ncloudcols = 1
+nlays = 30
 days = 5000 #model days
 min_press = 1.
 cloud_source = 1 #0 for manual, 1 for MISR
@@ -432,7 +430,7 @@ for ncols in ncolss:
     cld_taus = [9.9]
 
     # mixco2_prescribed_facs = np.array([0.03125,0.0625,0.125,0.25,0.5,1,2,4,8])
-    mixco2_prescribed_facs = np.array([1.0])
+    mixco2_prescribed_facs = np.array([1.0,2.0])
     # mixco2_prescribed_facs = np.array([0.03125])
 
     # psurf_overrides = [1000.,2000.]
@@ -445,7 +443,7 @@ for ncols in ncolss:
     lcs = np.linspace(10,3,1)
     lcs = lcs * -1.
 
-    lapse_types = [0]
+    lapse_types = [0,1]
 
     i_lt = 0
     for lapse_type in lapse_types:
@@ -551,7 +549,7 @@ for ncols in ncolss:
                                                     #lct = 250.0
                                                     #lcf = 0.5
                                                     #lcod = 5.0
-                                                    tp = 0.01
+                                                    tp = 5.0
                                                     #fth = np.zeros(ncols)
                                                     #for i in range(ncols):
                                                     #    fth[i] = 15.0 - abs(collats[i])/18.0
